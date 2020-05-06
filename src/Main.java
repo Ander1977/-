ccc62325c6533ab44b7865751a272d6504ddcc71
Andrei Kopatilov <koopa-andrej@live.com>
2020-05-06 17:38:14 +1000

    cod formatted

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 class Main {
-    public static
-    void main(String[] args) {
+    public static void main(String[] args) {
         long price = 5260;
-        int  bonus = 20;
+        int bonus = 20;
         long miles = price / bonus;
         System.out.println(miles);
     }
