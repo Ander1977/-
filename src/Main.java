70600382f4d5dd5800870d0564f26012bc5d3b05
Andrei Kopatilov <koopa-andrej@live.com>
2020-05-06 14:35:27 +1000

    First

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        long prise = 5260;
+        int bonus = 20;
+        long miles = prise / bonus;
+
+        System.out.println(miles);
+
+    }
+}
